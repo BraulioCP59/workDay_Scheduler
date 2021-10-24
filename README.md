17f5d23379fa0591f99f5502d62a302d44f3d013
Braulio Mora <brauliocdc59@gmail.com>
2021-10-23 23:42:51 -0700

    set up main time and date display

--- README.md
+++ README.md
@@ -17,15 +17,19 @@ SO THAT I can manage my time effectively
 ## Acceptance Criteria
 
 ```md
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
+GIVEN I am using a daily planner to create a schedule - Done
+WHEN I open the planner - Done
+THEN the current day is displayed at the top of the calendar - Done
+
 WHEN I scroll down
 THEN I am presented with timeblocks for standard business hours
+
 WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
+
+WHEN I click into a timeblock - Done
+THEN I can enter an event- Done
+
 WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage
 WHEN I refresh the page
--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,48 @@
-//start writing 
+//selectors
+var bodyEl = $("body");
+var pageHeaderEl = $("header");
+var appTitleHeader = $("h1");
+var appTaglineEl = $("#tagline");
+var currentDayTitleEl = $("#currentDay");
+var timeBlockContainerEl = $("#timeBlockContainer");
+
+
+
+//global declarations
+var currentTime;
+var timeBlockEl;
+
+//function to instantiate current time and display in date format on page
+function displayTime()
+{
+    var currentTime = dayjs();
+    console.log(currentTime);
+    currentDayTitleEl.text(currentTime.format("dddd, MMMM D YYYY"));
+
+}
+
+
+//
+function createTimeBlock()
+{
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "row");
+    var timeBlock = $(newDiv);
+    timeBlock.append("<div class='hour col-1'>100AM</div>");
+    timeBlock.append("<div class='form-floating col-10'><textarea class='form-control' placeholder='Enter Task Here' id='taskInput'></textarea></div>");
+    timeBlock.append("<button class='saveBtn col-1'>+Save+</button>");
+    
+    return timeBlock;
+}
+
+function renderTimeBlocks()
+{
+    displayTime();
+    for(var i = 0; i < 10; i++)
+    {
+        timeBlockContainerEl.append(createTimeBlock)
+    }
+}
+
+
+renderTimeBlocks();
