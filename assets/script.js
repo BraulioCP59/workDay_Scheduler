1be64a52b8e72c84aec4a1a1dd1ff2f3c19a0ef8
Braulio Mora <brauliocdc59@gmail.com>
2021-10-24 23:29:42 -0700

    completed logic for persisting tasks and updated readme

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,10 @@ var appTaglineEl = $("#tagline");
 var currentDayTitleEl = $("#currentDay");
 var timeBlockContainerEl = $("#timeBlockContainer");
 
-
-
 //global declarations
 var currentTime;
 var timeBlockEl;
+var taskListData = {};
 
 //function to instantiate current time and display in date format on page
 function displayTime()
@@ -21,28 +20,72 @@ function displayTime()
 
 }
 
+function renderPage()
+{
+    //
+    displayTime();
+
+    //
+    if(!window.localStorage.getItem("taskListData"))
+    {
+        window.localStorage.setItem("taskListData", JSON.stringify(taskListData));
+    }
+    
+
+    var tasks = JSON.parse(window.localStorage.getItem("taskListData"));
+    //
+    var currentHour = dayjs().hour();
+    console.log(currentHour);
+
+    //
+    for (var i = 9; i < 18; i++) {
+        $(".container").append(`
+        <div class="time-block row">
+          <div class="hour col-1">${
+            i < 12 ? `${i}AM` : i > 12 ? `${i - 12}PM` : "12PM"
+          }</div>
+          <textarea
+          name=""
+          id="${i}"
+          cols="30"
+          rows="3"
+          class="description col-10 ${
+            i < currentHour ? "past" : i === currentHour ? "present" : "future"
+          }"
+          >${tasks[i] || ""}</textarea
+          ><button class="saveBtn col-1" id="${i}">Save <i class="fas fa-save" id="${i}"></i></button>
+          </div>
+          `);
+      }
+
+
+      saveTask();
+
+}
 
 //
-function createTimeBlock()
+function saveTask()
 {
-    var newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "row");
-    var timeBlock = $(newDiv);
-    timeBlock.append("<div class='hour col-1'>100AM</div>");
-    timeBlock.append("<div class='form-floating col-10'><textarea class='form-control' placeholder='Enter Task Here' id='taskInput'></textarea></div>");
-    timeBlock.append("<button class='saveBtn col-1'>+Save+</button>");
-    
-    return timeBlock;
+    var tasks = JSON.parse(window.localStorage.getItem("taskListData"));
+
+    $(".saveBtn").on("click", (event)=>{
+        console.log(event.target);
+
+        if($(event.target).attr('class') == "saveBtn col-1")
+        {
+            console.log($(event.target).siblings('textarea').val());
+            console.log(event.target.id);
+            tasks[event.target.id] = $(event.target).siblings('textarea').val();
+
+        }else{
+            console.log($(event.target).parent().siblings('textarea').val());
+            tasks[event.target.id] = $(event.target).parent().siblings('textarea').val();
+            
+        }
+        console.log(tasks);
+        window.localStorage.setItem("taskListData", JSON.stringify(tasks));
+    })
 }
 
-function renderTimeBlocks()
-{
-    displayTime();
-    for(var i = 0; i < 10; i++)
-    {
-        timeBlockContainerEl.append(createTimeBlock)
-    }
-}
 
-
-renderTimeBlocks();
+renderPage();
